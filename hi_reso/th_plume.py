cc33adcd0ba7afca198a5ffe52e9cd01f2a68eb8
neko2048 <show.999991@gmail.com>
2021-05-22 20:29:38 +0800

    add drawing pure spatial mean

--- hi_reso/th_plume.py
+++ hi_reso/th_plume.py
@@ -11,27 +11,6 @@ sys.path.insert(1, '../') # move 2 parent folder (figures/)
 from function import progressbar
 from nclcmaps import nclcmap
 # ===========================================
-
-exp_name = str(input())
-td_loc = '/media/wk2/atmenu10246/VVM/DATA/' + exp_name + '/archive/'
-td_files, dy_files = [], []
-for td, dy in zip(glob.glob(td_loc +exp_name + '.L.Thermodynamic-??????.nc'), glob.glob(td_loc + exp_name + '.L.Dynamic-??????.nc')):
-    td_files.append(td)
-    dy_files.append(dy)
-print('Appended nc files: '+ str(len(td_files)))
-# ========================================
-
-##### universal variables #####
-dt = netCDF4.Dataset(td_files[0]) # take initial state
-z, y, x = np.array(dt['zc']), np.array(dt['yc']), np.array(dt['xc'])
-print(np.max(z))
-x = x - max(x)/2
-xslice = slice(0, len(x))
-z = z[z<=15000]
-th = dt['th'][0, :len(z), :, :]
-area = th[0, :, :] != th[0, 0, 0]
-area = np.tile(area, (len(z), 1, 1))
-# =============================
 def average(init, end):
     total = end - init
     thmean = np.zeros((len(z), ))
@@ -59,6 +38,7 @@ def draw_thss(init, end):
     t = int(np.array(dt['Time']))
 
     thss = np.zeros((end-init, len(z)))
+    buoss = np.zeros((end-init, len(z)))
     wss = np.zeros((end-init, len(z)))
     for tidx in range(init, end):
         td = netCDF4.Dataset(td_files[tidx])
@@ -68,15 +48,37 @@ def draw_thss(init, end):
         th = np.where(th == 0., np.nan, th)
         th = np.nanmean(th, axis=(1, 2))
         thss[tidx-init, :] = th - thbar
+        buoss[tidx-init, :] = (th - thbar) / thbar
 
         dy = netCDF4.Dataset(dy_files[tidx])
         w = np.mean(dy['w'][0, :len(z)] * area, axis=(1, 2))
         wss[tidx-init, :] = w
     
+    # ============= draw th spatial mean ================
     pcolormesh(np.arange(init, end), z, thss.transpose(), 
-               shading='near', cmap='coolwarm')
+               shading='near', cmap='coolwarm', vmin=-1., vmax=1.)
     colorbar()
+    xlabel('Time (min)', fontsize=12); ylabel('Height (m)', fontsize=12)
+    title(r"$\theta '$ in spatial average")
     savefig('thss.jpg', dpi=300)
+    clf()
+    # ============= draw w spatial mean ================
+    pcolormesh(np.arange(init, end), z, wss.transpose(), 
+               shading='near', cmap='coolwarm', vmin=-0.08, vmax=0.08)
+    colorbar()
+    xlabel('Time (min)', fontsize=12); ylabel('Height (m)', fontsize=12)
+    title(r"W in spatial average")
+    savefig('wss.jpg', dpi=300)
+    clf()
+    # ============= draw w spatial mean ================
+    pcolormesh(np.arange(init, end), z, buoss.transpose(),
+               shading='near', cmap='coolwarm', vmin=-0.004, vmax=0.004)
+    colorbar()
+    xlabel('Time (min)', fontsize=12); ylabel('Height (m)', fontsize=12)
+    title(r"Buoyancy in spatial average")
+    savefig('buoss.jpg', dpi=300)
+    clf()
+
 def draw_thp(init, end):
     """draw th in average"""
     ##### retrieve variables #####
@@ -113,6 +115,28 @@ def draw_thp(init, end):
     clf()
 
 if __name__ == '__main__':
+
+    exp_name = str(input())
+    td_loc = '/media/wk2/atmenu10246/VVM/DATA/' + exp_name + '/archive/'
+    td_files, dy_files = [], []
+    for td, dy in zip(glob.glob(td_loc +exp_name + '.L.Thermodynamic-??????.nc'), glob.glob(td_loc + exp_name + '.L.Dynamic-??????.nc')):
+        td_files.append(td)
+        dy_files.append(dy)
+    print('Appended nc files: '+ str(len(td_files)))
+    # ========================================
+    
+    ##### universal variables #####
+    dt = netCDF4.Dataset(td_files[0]) # take initial state
+    z, y, x = np.array(dt['zc']), np.array(dt['yc']), np.array(dt['xc'])
+    print(np.max(z))
+    x = x - max(x)/2
+    xslice = slice(0, len(x))
+    z = z[z<=15000]
+    th = dt['th'][0, :len(z), :, :]
+    area = th[0, :, :] != th[0, 0, 0]
+    area = np.tile(area, (len(z), 1, 1))
+    # =============================
+
     init, end = 0, 40 # from bubble up to dissipated
     draw_thss(init, end)
     #draw_thp(init, end)
